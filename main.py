7f68faf7f1358b2bf3160cfcfe405a9a64c09bd2
amura16 <roddytanjaka16@gmail.com>
2025-12-03 09:46:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 import openai
+import sys
 # Rediriger les erreurs vers stdout pour les logs Apache
 sys.stderr = sys.stdout
 
