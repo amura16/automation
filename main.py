4a9f2eb10eb8ca2f3e0144d1ad3bd4d30ee16ceb
amura16 <roddytanjaka16@gmail.com>
2025-12-04 14:53:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import requests
 import json
 from google import genai
 import re
-from dotenv import load_dotenv
 import os
 
 sys.stderr = sys.stdout
@@ -25,11 +24,7 @@ CORS(app)
 BASE_URL = "https://www.agent-ia-supportpresta.omega-connect.com"
 API_KEY = "4BC8AHKVG3RB5K3AH2DK82SL5SQMJB9H"
 
-load_dotenv()  # lit le fichier .env
-key = os.getenv("API_KEY")
-
-# client = genai.Client(api_key="AIzaSyDSgNUQke1A6QRkaahNSrpqVvJNo2RV5cA") 
-client = genai.Client(api_key=key) 
+client = genai.Client(api_key="AIzaSyDSgNUQke1A6QRkaahNSrpqVvJNo2RV5cA") 
 
 # prompt
 
